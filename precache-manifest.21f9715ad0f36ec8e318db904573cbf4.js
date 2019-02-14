080c7f1a754cfda41c1b72b3a5bfd1faac24273b
pablowbk <pablo.weisbek@gmail.com>
2019-02-13 23:05:36 -0300

    Updates

---
+++ precache-manifest.21f9715ad0f36ec8e318db904573cbf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "70b082353b686a494e27",
+    "url": "/weather-app/static/js/main.70b08235.chunk.js"
+  },
+  {
+    "revision": "4cb7f3436a37ae96689b",
+    "url": "/weather-app/static/js/2.4cb7f343.chunk.js"
+  },
+  {
+    "revision": "70b082353b686a494e27",
+    "url": "/weather-app/static/css/main.5cad7c7a.chunk.css"
+  },
+  {
+    "revision": "4cb7f3436a37ae96689b",
+    "url": "/weather-app/static/css/2.77deadee.chunk.css"
+  },
+  {
+    "revision": "9381e21ab0d85122b90fe332dc6b0d5f",
+    "url": "/weather-app/index.html"
+  }
+];
