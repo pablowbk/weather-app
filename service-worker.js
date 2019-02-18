49e8cc7b4d1338373c92cddd30609310961310de
pablowbk <pablo.weisbek@gmail.com>
2019-02-18 14:26:21 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.cd8c3ca55717d1fd5ba0add31e45900e.js"
+  "/weather-app/precache-manifest.6a107460fc4bb5bf632267419c6792c6.js"
 );
 
 workbox.clientsClaim();
